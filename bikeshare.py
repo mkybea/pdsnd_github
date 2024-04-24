25505e5b71914f9dfc2e3e807133643969cce92b
Katie Beach <mky.beach@gmail.com>
2024-04-23 23:15:29 -0400

    Adjusted timing of print statements for better readability.

--- bikeshare.py
+++ bikeshare.py
@@ -6,7 +6,7 @@ import time
 
 def print_p(statement):
     print(statement)
-    time.sleep(.500)
+    time.sleep(1)
 
 # Some lambda helper functions
 get_value = lambda x,y,n :x.groupby([y])[y].count().sort_values(ascending=False).head(n).values
